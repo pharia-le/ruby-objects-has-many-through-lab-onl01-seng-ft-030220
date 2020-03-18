38a970a1ca7f191d2788ef7d2aa8b1580e72adce
Pharia Le <pharia.le@gmail.com>
2020-03-18 06:43:28 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -10,7 +10,7 @@ class Patient
   end
   
   def new_appointment(doctor, date)
-    appointment = Appointment.new(   )  
+    appointment = Appointment.new(date, self, doctor)  
   end
   
   def appointments
